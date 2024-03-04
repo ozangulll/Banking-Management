2593ee7f7b18df40fe6d30381e9bd41575689505
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-03-04 19:38:45 +0300

    startapp created and changed.

--- src/main/java/com/sau/bankingmanagement/StartApplication.java
+++ src/main/java/com/sau/bankingmanagement/StartApplication.java
@@ -1,20 +1,39 @@
 package com.sau.bankingmanagement;
 
+import com.sau.bankingmanagement.controller.CustomerController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class StartApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        TabPane tabPane = new TabPane();
+
+        Tab customerTab = new Tab("Customer");
+        FXMLLoader customerLoader = new FXMLLoader(StartApplication.class.getResource("customer-view.fxml"));
+        customerTab.setContent(customerLoader.load());
+        tabPane.getTabs().add(customerTab);
+
+        Tab accountTab = new Tab("Account");
+        FXMLLoader accountLoader = new FXMLLoader(StartApplication.class.getResource("account-view.fxml"));
+        accountTab.setContent(accountLoader.load());
+
+        tabPane.getTabs().add(accountTab);
+
+
+        Scene scene = new Scene(tabPane, 640, 480);
+        primaryStage.setTitle("Banking Management");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        primaryStage.setMaxWidth(566);
+        primaryStage.setMaxHeight(416);
     }
 
     public static void main(String[] args) {
